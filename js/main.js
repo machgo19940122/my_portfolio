87ff963aabe07b5cd66c0b50732601704587d2ab
machgo19940122 <machgo122@gmail.com>
2022-08-31 11:55:36 +0900

    ハンバーガーメニュー修正

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(function(){
+  $(".more-1").on("click", function() {
+    $(this).toggleClass("on-click");
+    $(".txt-hide-1").slideToggle(1000);
+  });
+}); 
+
+
+$(function(){
+  $(".more-2").on("click", function() {
+    $(this).toggleClass("on-click");
+    $(".txt-hide-2").slideToggle(1000);
+  });
+}); 
+
+
+
+$(function(){
+  $(".more-3").on("click", function() {
+    $(this).toggleClass("on-click");
+    $(".txt-hide-3").slideToggle(1000);
+  });
+}); 
